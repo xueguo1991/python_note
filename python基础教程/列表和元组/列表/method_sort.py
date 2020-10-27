d6697e5799e78f48c1a1c404866417b3860b415a
wanbo <xueguo1991@foxmail.com>
2020-10-27 23:03:54 +0800

    排序相关的探究

--- python基础教程/列表和元组/列表/method_sort.py
+++ python基础教程/列表和元组/列表/method_sort.py
@@ -16,3 +16,24 @@ arr = ['a', 'b', 'c', 'abc', 'ab1', 'ab', '1', '2', '3','100', '10']
 arr.sort()
 # 对字符串排序时是字典序
 print(arr)
+
+arr = ['a', 'b', 'c', 'abc', 'ab1', 'ab']
+arr_sorted = sorted(arr)
+# 对序列排序
+print(arr)
+print(arr_sorted)
+
+arr = ['abc','ab', 'xy', '1', '666zz', 'zzz', 'i中国', '世界']
+arr.sort(key=len)
+# 使用关键字参数 len 指定按照长度排列
+print(arr)
+
+arr = ['abc','ab', 'xy', '1', '333']
+arr.sort(reverse=True)
+# 字典序倒叙排列
+print(arr)
+
+arr = ['abc','ab', 'xy', '1', '333']
+arr.sort(reverse=False)
+# 字典序正序排列
+print(arr)
