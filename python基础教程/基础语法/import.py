d8aa4c5cb424c6c46fe589ed17b90793203c8fa0
wanbo <xueguo1991@foxmail.com>
2020-11-02 23:10:23 +0800

    import and unpacking

--- python基础教程/基础语法/import.py
+++ python基础教程/基础语法/import.py
@@ -7,10 +7,18 @@
 # print(sqrt(5))
 # NameError: name 'sqrt' is not defined
 
-
+# 直接导入包中所有
 import math
 print(math.sqrt(2))
 
-
+# 导入包中某个函数
 from math import log10
 print(log10(100))
+
+# 导入包时重命名
+import math as m
+print(m.log(5, 2))
+
+# 导入包中函数时重命名
+from math import log as l
+print(l(2, 5))
